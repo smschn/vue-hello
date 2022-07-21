dccd331812c0b72bc242bf7dbbc54f7c105527e1
Luca Somaschini <somas99@gmail.com>
2022-07-21 16:32:12 +0200

    Bonus files separated from basic exercise files

---
+++ Bonus/js/main.js
@@ -0,0 +1,10 @@
+// creo istanza di Vue
+var app = new Vue (
+    {
+        el: '#prova',
+        data: {
+            message: 'Bonus: secondi passi con Vue',
+            myImgSrc: 'https://picsum.photos/400/400'
+        }
+    }
+);
--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var app = new Vue (
         el: '#prova',
         data: {
             message: 'Primi passi con Vue',
-            myImgSrc: 'https://picsum.photos/400/400'
         }
     }
 );
